e3902645e1b79074fb4996d8780d765922115101
V <V@As-iMac-3.local>
2014-05-03 02:47:20 -0400

    na

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express"),
  	app = express(),
  	http = require('http');
 
+ var Browser = require('zombie');
+ var assert = require('assert');
 // configuration =====================
 app.use(express.static(__dirname + '/public'));  // set static files location /public/img for users
 
@@ -11,10 +13,14 @@ app.get('/', function(req, res){
 	res.sendfile('public/views/index.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
-// listen 
+//listen 
 var port = Number(process.env.PORT || 5000);
 http.createServer(app).listen(port, function(){
 	console.log("Listening on " + port);
 });
 
 
+
+Browser.visit('http://zombie.labnotes.org/', function(e,browser){
+	console.log(browser.site);
+});
